6384e253250802fbcaca1271c75a6d972d211df6
Box. <162526757+Aguirre5@users.noreply.github.com>
2026-03-26 09:52:18 -0300

    Add palindrome verification function in LAB314.py
    s

---
+++ LAB314.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+# Verificacion palíndroma.
+def palindromia(text):
+    normaltext = text.replace(" ", "").lower()
+    return normaltext == normaltext[::-1]
+
+text = input("Pon una frase: ")
+
+if palindromia(text):
+    print("La frase es palíndroma.")
+else:
+    print("El texto no es palíndromo.")
